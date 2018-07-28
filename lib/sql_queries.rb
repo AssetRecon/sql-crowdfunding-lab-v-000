b4333deb8d3b304053637349a87d2f334fa4f193
Edsel Caprice <caprice@mail.usf.edu>
2018-07-28 16:35:00 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -28,7 +28,8 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 from projects
 inner join pledges
 on projects.id = pledges.project_id
-group by projects.title"
+group by projects.title
+where projects.title having sum(pledges.amount) - projects.funding_goal > 0  "
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
